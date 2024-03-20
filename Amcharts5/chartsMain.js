66d0303cb13ec3d4405e26b0ca6a713387b3c59e
Sanath Ezhuthachan <90824508+Sanath710@users.noreply.github.com>
2024-03-20 15:24:37 +0530

    Add files via upload

---
+++ Amcharts5/chartsMain.js
@@ -0,0 +1,219 @@
+var getScriptPromisify = src => {
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
+
+var data1 = undefined;
+;(function (
+    root = undefined,
+    chart = undefined,
+    xAxis = undefined,
+    yAxis = undefined,
+    series1 = undefined,
+    series2 = undefined,
+    legend = undefined,
+    dim = undefined,
+    mes1 = undefined,
+    mes2 = undefined) {
+        
+  const prepared = document.createElement('template')
+  prepared.innerHTML = `
+              <style>
+              </style>
+              <div id="root" style="width: 100%; height: 100%;">
+              </div>
+            `
+
+  class customXYChart extends HTMLElement {
+    constructor () {
+      //   console.clear()
+      super()
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+      this._root = this._shadowRoot.getElementById('root')
+      this._props = {}
+      //   this.render()
+    }
+
+    onCustomWidgetBeforeUpdate (changedProperties) {
+      this._props = { ...this._props, ...changedProperties }
+    }
+
+    onCustomWidgetAfterUpdate (changedProperties) {
+      //   console.log(changedProperties)
+      this._myDataBinding = changedProperties['myDataBinding']
+      console.log(this._myDataBinding)
+      this.render()
+    }
+
+    async render () {
+      if (this._myDataBinding.state !== 'success') {
+        return
+      }
+
+      dim =
+        this._myDataBinding['metadata']['dimensions']['dimensions_0']
+          .description
+      mes1 =
+        this._myDataBinding['metadata']['mainStructureMembers']['measures_0']
+          .label
+      mes2 =
+        this._myDataBinding['metadata']['mainStructureMembers']['measures_1']
+          .label
+
+      var data = []
+      for (var i = 0; i < this._myDataBinding['data'].length; i++) {
+        var obj = {}
+        obj[dim] = this._myDataBinding['data'][i]['dimensions_0'].label
+        obj[mes1] = this._myDataBinding['data'][i]['measures_0'].raw
+        obj[mes2] = this._myDataBinding['data'][i]['measures_1'].raw
+        data.push(obj)
+      }
+
+      console.log(data)
+
+      // Define data
+      //   var data = [
+      //     {
+      //       category: 'Research',
+      //       value1: 1000,
+      //       value2: 588
+      //     },
+      //     {
+      //       category: 'Marketing',
+      //       value1: 1200,
+      //       value2: 1800
+      //     },
+      //     {
+      //       category: 'Sales',
+      //       value1: 850,
+      //       value2: 1230
+      //     }
+      //   ]
+
+      if (!root) {
+
+        await getScriptPromisify('//cdn.amcharts.com/lib/5/index.js');
+        await getScriptPromisify('//cdn.amcharts.com/lib/5/xy.js');
+        await getScriptPromisify('//cdn.amcharts.com/lib/5/percent.js');
+
+        root = am5.Root.new(this._root)
+        console.log('root')
+        console.log(root)
+
+        console.log('Data')
+        console.log(data)
+
+        dim =
+          this._myDataBinding['metadata']['dimensions']['dimensions_0']
+            .description
+        mes1 =
+          this._myDataBinding['metadata']['mainStructureMembers']['measures_0']
+            .label
+        mes2 =
+          this._myDataBinding['metadata']['mainStructureMembers']['measures_1']
+            .label
+
+        chart = root.container.children.push(
+          am5xy.XYChart.new(root, {
+            panY: false,
+            wheelY: 'zoomX',
+            layout: root.verticalLayout
+          })
+        )
+
+        // Craete Y-axis
+        yAxis = chart.yAxes.push(
+          am5xy.ValueAxis.new(root, {
+            renderer: am5xy.AxisRendererY.new(root, {})
+          })
+        )
+
+        // Create X-Axis
+        xAxis = chart.xAxes.push(
+          am5xy.CategoryAxis.new(root, {
+            maxDeviation: 0.2,
+            renderer: am5xy.AxisRendererX.new(root, {}),
+            categoryField: dim
+          })
+        )
+
+        // Create series
+        series1 = chart.series.push(
+          am5xy.ColumnSeries.new(root, {
+            name: 'Series',
+            xAxis: xAxis,
+            yAxis: yAxis,
+            valueYField: mes1,
+            categoryXField: dim,
+            tooltip: am5.Tooltip.new(root, {})
+          })
+        )
+
+        series2 = chart.series.push(
+          am5xy.ColumnSeries.new(root, {
+            name: 'Series 1 ',
+            xAxis: xAxis,
+            yAxis: yAxis,
+            valueYField: mes2,
+            categoryXField: dim
+          })
+        )
+
+        legend = chart.children.push(am5.Legend.new(root, {}))
+
+        chart = root.container.children.push(
+          am5percent.PieChart.new(root, {})
+        )
+
+
+        // Define data
+data1 = [{
+  country: "France",
+  sales: 100000
+}, {
+  country: "Spain",
+  sales: 160000
+}, {
+  country: "United Kingdom",
+  sales: 80001
+}];
+
+// Create series
+var series3 = chart.series.push(
+  am5percent.PieSeries.new(root, {
+    name: "Series 3",
+    valueField: "sales",
+    categoryField: "country"
+  })
+);
+
+series3.data.setAll(data1);
+
+      }
+
+      console.log(root)
+
+      console.log(dim)
+      console.log(mes1)
+      console.log(mes2)
+
+      xAxis.data.setAll(data)
+
+      series1.data.setAll(data)
+
+      series2.data.setAll(data)
+
+series3.data.setAll(data1);
+
+console.log(chart);
+
+
+
+      // Add legend
+      legend.data.setAll(chart.series.values)
+    }
+  }
+  customElements.define('cw-xy-chart', customXYChart)
+})()
---
+++ D3 JS/d3Main.js
@@ -0,0 +1,131 @@
+var getScriptPromisify = src => {
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
+;(function () {
+  const prepared = document.createElement('template')
+  prepared.innerHTML = `
+                <style>
+                </style>
+                <div id="root" style="width: 100%; height: 100%;">
+                <button onclick="update('var1')">Variable 1</button>
+<button onclick="update('var2')">Variable 2</button>
+                </div>
+              `
+
+  class d3BarChart extends HTMLElement {
+    constructor () {
+      //   console.clear()
+      super()
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+      this._root = this._shadowRoot.getElementById('root')
+      this._props = {}
+
+    }
+
+    onCustomWidgetBeforeUpdate (changedProperties) {
+      this._props = { ...this._props, ...changedProperties }
+    }
+
+    onCustomWidgetAfterUpdate (changedProperties) {
+      this._myDataBinding = changedProperties['myDataBinding']
+      console.log(this._myDataBinding)
+      this.render()
+    }
+
+    async render () {
+
+      await getScriptPromisify('https://d3js.org/d3.v4.js')
+
+      if(this._myDataBinding.state != 'success') {
+        return;
+      }
+
+      var data = [];
+
+      for (var i = 0; i < this._myDataBinding['data'].length; i++) {
+        var obj = {}
+        obj['group'] = this._myDataBinding['data'][i]['dimensions_0'].label
+        obj['var1'] = this._myDataBinding['data'][i]['measures_0'].raw
+        obj['var2'] = this._myDataBinding['data'][i]['measures_1'].raw
+        data.push(obj)
+      }
+
+      console.log(data)
+
+      var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom
+
+      // append the svg object to the body of the page
+      var svg = d3
+        .select(this._root)
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+      // Initialize the X axis
+      var x = d3.scaleBand().range([0, width]).padding(0.2)
+      var xAxis = svg
+        .append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+
+      // Initialize the Y axis
+      var y = d3.scaleLinear().range([height, 0])
+      var yAxis = svg.append('g').attr('class', 'myYaxis')
+
+      // A function that create / update the plot for a given variable:
+      function update (selectedVar) {
+        // Parse the Data
+       console.log(selectedVar);
+
+            x.domain(
+              data.map(function (d) {
+                return d.group
+              })
+            )
+            xAxis.transition().duration(1000).call(d3.axisBottom(x))
+
+            // Add Y axis
+            y.domain([
+              0,
+              d3.max(data, function (d) {
+                return d[selectedVar]
+              })
+            ])
+            yAxis.transition().duration(1000).call(d3.axisLeft(y).tickFormat(function(d){return d/100000 + " Million"}))
+
+            // variable u: map data to existing bars
+            var u = svg.selectAll('rect').data(data)
+
+            // update bars
+            u.enter()
+              .append('rect')
+              .merge(u)
+              .transition()
+              .duration(1000)
+              .attr('x', function (d) {
+                return x(d.group)
+              })
+              .attr('y', function (d) {
+                return y(d[selectedVar])
+              })
+              .attr('width', x.bandwidth())
+              .attr('height', function (d) {
+                return height - y(d[selectedVar])
+              })
+              .attr('fill', '#69b3a2')
+      }
+
+      window.update = update;
+
+      // Initialize plot
+      update('var1')
+    }
+  }
+  customElements.define('cw-d3-barchart', d3BarChart)
+})()
